fe3fce010d1faa449d6c4016e3c814c508e4c9e8
Shivam1097 <shivam.dev1097@gmail.com>
2019-06-09 19:15:30 +0530

    Initial commit

---
+++ Buttons_and_Interactivity/app/src/main/java/shivam/com/MainActivity.java
@@ -0,0 +1,21 @@
+package shivam.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void click(View v){
+        // It prints the line in the logcat console.
+        Log.i("info","Button Pressed");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
