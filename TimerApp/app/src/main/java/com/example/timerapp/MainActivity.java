968df6d3308c47a1cd2fed24ed08a68d47ab9419
Shivam1097 <shivam.dev1097@gmail.com>
2019-08-04 20:01:28 +0530

    Initial Commit

--- TimerApp/app/src/main/java/com/example/timerapp/MainActivity.java
+++ TimerApp/app/src/main/java/com/example/timerapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.timerapp;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -11,20 +14,45 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView ;
     SeekBar seekBar;
+    Button goButton;
+    Boolean isCounterActive;
+    CountDownTimer timer;
 
+
+    public void resetTimer(){
+        seekBar.setProgress(10);
+        textView.setText("00:10");
+        seekBar.setEnabled(true);
+        timer.cancel();
+        goButton.setText("Go !");
+        isCounterActive=false;
+    }
 
     public void startTimer(View view){
-        CountDownTimer timer = new CountDownTimer(seekBar.getProgress()*1000,1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                updateTime((int)millisUntilFinished/1000);
-            }
 
-            @Override
-            public void onFinish() {
+        if (isCounterActive){
+            resetTimer();
 
-            }
-        }.start();
+        }else {
+
+            isCounterActive=true;
+            seekBar.setEnabled(false);
+            goButton.setText("Stop !");
+            timer= new CountDownTimer(seekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    //Log.i("Status", Integer.toString(seekBar.getProgress() * 1000-(int)millisUntilFinished));
+                    updateTime((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer player = MediaPlayer.create(getApplicationContext(), R.raw.air);
+                    player.start();
+                    resetTimer();
+                }
+            }.start();
+        }
     }
 
 
@@ -51,11 +79,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         seekBar= findViewById(R.id.seekBar);
         textView=findViewById(R.id.textView);
+        goButton=findViewById(R.id.button);
         seekBar.setMax(600);
         seekBar.setProgress(10);
         textView.setText("00:10");
+
+        isCounterActive=false;
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
