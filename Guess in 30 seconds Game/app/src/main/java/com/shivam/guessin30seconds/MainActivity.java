1c1e0c2d7448d33fce8617ba72a381980af25035
Shivam1097 <shivam.dev1097@gmail.com>
2019-08-05 23:32:36 +0530

    Initial Commit

--- Guess in 30 seconds Game/app/src/main/java/com/shivam/guessin30seconds/MainActivity.java
+++ Guess in 30 seconds Game/app/src/main/java/com/shivam/guessin30seconds/MainActivity.java
@@ -1,5 +1,7 @@
 package com.shivam.guessin30seconds;
 
+import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,35 +14,58 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    TextView resultStatus;
+    TextView equation;
+    TextView scoreStatus;
+    TextView timer;
+    Button playAgain;
+    Button op1;
+    Button op2;
+    Button op3;
+    Button op4;
+
+    ConstraintLayout gameSetupLayout;
+
     String equationString ;
     int result ;
     int indexOfCorrectAnswer ;
+    int score=0;
+    int numberOfQuestionsDisplayed=0;
+    boolean gameOver=false;
     ArrayList<Integer> options =new ArrayList<>();
 
 
     public void start(View view){
         startButton.setVisibility(View.INVISIBLE);
+        gameSetupLayout.setVisibility(View.VISIBLE);
+        setUpNewQuestion();
+
+        startTimer();
     }
 
 
     public void optionPressed(View view){
 
-        int tag = (Integer)view.getTag();
-        
+        if (!gameOver) {
+            if (Integer.toString(indexOfCorrectAnswer).equals(view.getTag().toString())) {
+                resultStatus.setText("Correct !");
+                score++;
+            } else
+                resultStatus.setText("Wrong! Try again:(");
+
+            numberOfQuestionsDisplayed++;
+            resultStatus.setVisibility(View.VISIBLE);
+            scoreStatus.setText(score + "/" + numberOfQuestionsDisplayed);
+            setUpNewQuestion();
+        }
+
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void setUpNewQuestion(){
 
-        Button op1= findViewById(R.id.option1);
-        Button op2= findViewById(R.id.option2);
-        Button op3= findViewById(R.id.option3);
-        Button op4= findViewById(R.id.option4);
 
-        TextView equation = findViewById(R.id.equation);
 
+        playAgain.setVisibility(View.INVISIBLE);
         Random random =new Random();
 
         int equationType=random.nextInt(2);  // Generates Random number 0,1
@@ -72,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
         indexOfCorrectAnswer=random.nextInt(4);  // Location of correct ans in List.
 
+        options.clear();
         for (int i=0; i<4; i++){
 
             if (i == indexOfCorrectAnswer)
@@ -104,8 +130,67 @@ public class MainActivity extends AppCompatActivity {
         op3.setText(Integer.toString(options.get(2)));
         op4.setText(Integer.toString(options.get(3)));
 
+    }
+
+
+    // Play again Logic!!!!
+    public void setPlayAgain(View view){
+        score=0;
+        numberOfQuestionsDisplayed=0;
+        gameOver=false;
+        timer.setText("30s");
+        playAgain.setVisibility(View.INVISIBLE);
+        scoreStatus.setText(score + "/" + numberOfQuestionsDisplayed);
+        setUpNewQuestion();
+        startTimer();
+    }
+
+
+
+    //Timer setUp
+    private void startTimer(){
+        new CountDownTimer(30050,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timer.setText(millisUntilFinished/1000 + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                playAgain.setVisibility(View.VISIBLE);
+                resultStatus.setVisibility(View.INVISIBLE);
+                gameOver=true;
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        op1=findViewById(R.id.option1);
+        op2= findViewById(R.id.option2);
+        op3= findViewById(R.id.option3);
+        op4= findViewById(R.id.option4);
+
+        equation= findViewById(R.id.equation);
+        resultStatus=findViewById(R.id.resultStatus);
+        resultStatus.setVisibility(View.INVISIBLE);
 
         startButton=findViewById(R.id.startButton);
+        startButton.setVisibility(View.VISIBLE);
+
+        scoreStatus=findViewById(R.id.scoreStatus);
+        playAgain = findViewById(R.id.playAgain);
+
+        timer=findViewById(R.id.timer);
+        gameSetupLayout=findViewById(R.id.gameSetupLayout);
+
+        gameSetupLayout.setVisibility(View.INVISIBLE);
+
+
 
     }
 }
